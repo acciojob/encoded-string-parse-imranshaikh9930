06a40e841e1d5edc6e162fde5be0014ed299b30e
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-22 15:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const parseCode = (str) => {
   // your code here
+  const arr = str.split('0');
+  
+let arrays = arr.filter((element) => element !== "");
+ 
+const firstName = arrays[0];
+const lastName = arrays[1];
+const id = arrays[2];
+
+return {
+  firstName,
+  lastName,
+  id
+};
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
+const str = prompt("Enter str: ")
 alert(JSON.stringify(parseCode(str)));
